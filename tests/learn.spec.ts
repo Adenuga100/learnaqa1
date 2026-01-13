663c6142d51107a02ddbac58d5452a97e773f6b0
Adenuga100 <adenugaadeyemiisaac@gmail.com>
2026-01-13 01:55:37 -0800

    dashboard

--- tests/learn.spec.ts
+++ tests/learn.spec.ts
@@ -23,37 +23,38 @@ import { DynamicElementPage } from "../pages/DynamicElement.po";
 //     await expect(sigUpPage.getTittle(page)).toBeVisible();
 // });
 
-test.describe('Unauthenticated tests', () => {
- test.use({ storageState: 'cookies.json' });
+// test.describe('Unauthenticated tests', () => {
+//  test.use({ storageState: 'cookies.json' });
 
- test('Sign Up functionality', async ({ page }) => {
-    // test.use({ storageState: { cookies: [], origins: [] } });
-    //   const context = await browser.newContext(); // ❌ no storageState
-//    const page = await context.newPage();
- //   test.use({ storageState: undefined });
+//  test('Sign Up functionality', async ({ page }) => {
+//     // test.use({ storageState: { cookies: [], origins: [] } });
+//     //   const context = await browser.newContext(); // ❌ no storageState
+// //    const page = await context.newPage();
+//  //   test.use({ storageState: undefined });
+//     await page.goto('/'); // Replace with your application's URL
+//     await HomePage.clickAcceptAllCookies(page);
+//     await HomePage.sigUp(page);
+//     await sigUpPage.fillFullNameAs(page, 'John Doe');
+//     await sigUpPage.fillEmailAs(page);
+//     await sigUpPage.fillPasswordAs(page);
+//     await sigUpPage.clickSignUpButton(page);
+//     await expect(sigUpPage.getTittle(page)).toBeVisible();
+// });
+
+// test('Sign In functionality', async ({ page }) => {
+//   await page.goto('/'); // Replace with your application's URL
+//   await HomePage.clickAcceptAllCookies(page);
+//   await signInPage.fillEmailAs(page, 'adenugaadeyemiisaac@gmail.com');
+//   await signInPage.fillPasswordAs(page, 'Hardayemmh4$');
+//   await signInPage.clickSignInButton(page);
+//   await expect(signInPage.getTittle(page)).toBeVisible();
+// });
+// });
+
+
+
+test('Sign In functionality', async ({ page }) => {
     await page.goto('/'); // Replace with your application's URL
-    await HomePage.clickAcceptAllCookies(page);
-    await HomePage.sigUp(page);
-    await sigUpPage.fillFullNameAs(page, 'John Doe');
-    await sigUpPage.fillEmailAs(page);
-    await sigUpPage.fillPasswordAs(page);
-    await sigUpPage.clickSignUpButton(page);
-    await expect(sigUpPage.getTittle(page)).toBeVisible();
-});
-
-test('Sign In functionality', async ({ page }) => {
-  await page.goto('/'); // Replace with your application's URL
-  await HomePage.clickAcceptAllCookies(page);
-  await signInPage.fillEmailAs(page, 'adenugaadeyemiisaac@gmail.com');
-  await signInPage.fillPasswordAs(page, 'Hardayemmh4$');
-  await signInPage.clickSignInButton(page);
-  await expect(signInPage.getTittle(page)).toBeVisible();
-  });
-});
-
-
-test('Sign In functionality', async ({ page }) => {
-    await page.goto(''); // Replace with your application's URL
     await expect(signInPage.getTittle(page)).toBeVisible();
 });
 
@@ -62,13 +63,15 @@ test('Sign In functionality', async ({ page }) => {
 test('Drag and Drop functionality on the Dashboard page', async ({ page }) => {
     
     await page.goto('');
+    await HomePage.clickDashboard(page);
     await DashboardPage.clickStartPracticeByIndex(page, 0);
     await DashboardPage.dragAndDrop(page);
     await expect(DashboardPage.getTittle(page)).toBeVisible();
 });
 
 test('Drag and Drop functionality on the slide menus', async ({ page }) => {
-    await page.goto('/dashboard');
+    await page.goto('');
+    await HomePage.clickDashboard(page);
     await DashboardPage.clickSideMenuByNameAs(page, 'Drag and Drop');
     // await DashboardPage.clickStartPracticesAs(page, 0);
     await DashboardPage.dragAndDrop(page);
@@ -76,14 +79,16 @@ test('Drag and Drop functionality on the slide menus', async ({ page }) => {
 });
 
 test('Dynamic Elements functionality click delayed element on Dashboard', async ({ page }) => {
-    await page.goto('/dashboard');
+    await page.goto('');
+    await HomePage.clickDashboard(page);
     await DashboardPage.clickStartPracticeByIndex(page, 1);
     await DynamicElementPage.clickDelayedElement(page);
     await expect(DynamicElementPage.getTittle(page)).toBeVisible({timeout: 10000});
 });
 
 test('Dynamic Elements functionality click delayed element on slide menus', async ({ page }) => {
-    await page.goto('/dashboard');
+    await page.goto('');
+    await HomePage.clickDashboard(page);
     // await DashboardPage.clickStartPracticesAs(page, 1);
     await DashboardPage.clickSideMenuByNameAs(page, 'Dynamic Elements');
     await DynamicElementPage.clickDelayedElement(page);
@@ -91,7 +96,8 @@ test('Dynamic Elements functionality click delayed element on slide menus', asyn
 });
 
 test('Dynamic Elements functionality click ajax data loading element on dashboard', async ({ page }) => {
-    await page.goto('/dashboard');
+    await page.goto('');
+    await HomePage.clickDashboard(page);
     await DashboardPage.clickStartPracticeByIndex(page, 1);
     // await DashboardPage.clickSideMenuByNameAs(page, 'Dynamic Elements');
     await DynamicElementPage.clickAjaxDataLoading(page);
@@ -100,16 +106,12 @@ test('Dynamic Elements functionality click ajax data loading element on dashboar
 });
 
 test('Dynamic Elements functionality click ajax data loading element on slide menus', async ({ page }) => {
-    await page.goto('/dashboard');
+    await page.goto('');
+    await HomePage.clickDashboard(page);
     //await DashboardPage.clickStartPracticesAs(page, 1);
     await DashboardPage.clickSideMenuByNameAs(page, 'Dynamic Elements');
     await DynamicElementPage.clickAjaxDataLoading(page);
     await DynamicElementPage.scrolldownonAjaxDataLoading(page);
     await expect(DynamicElementPage.getAjaxDataLoadingTittle(page)).toBeVisible({timeout: 10000});
 });
-
-
-
-
-
 
